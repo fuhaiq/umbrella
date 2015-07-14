e0108c117116c9c017c2e181d3d07ee2f9d092a8
fuhaiq@gmail.com <fuhaiq@gmail.com>
2015-07-14 09:47:33 +0800

    test change

--- umbrella/src/main/java/com/umbrella/mongo/MongoModule.java
+++ umbrella/src/main/java/com/umbrella/mongo/MongoModule.java
@@ -24,6 +24,11 @@ public class MongoModule extends AbstractModule{
 		MongoInterceptor mongoInterceptor = new MongoInterceptor();
 		requestInjection(mongoInterceptor);
 		
+		
+		
+		
+		
+		
 		bindInterceptor(Matchers.any(), Matchers.annotatedWith(MongoCycle.class), mongoInterceptor);
 	}
 
