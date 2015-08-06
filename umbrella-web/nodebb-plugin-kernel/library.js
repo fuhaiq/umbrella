d5c60d91f6e7487c374c16c99771cdcf3143f7d2
fuhaiq <fuhaiq@gmail.com>
2015-08-06 18:21:34 +0800

    add topic create job

--- umbrella-web/nodebb-plugin-kernel/library.js
+++ umbrella-web/nodebb-plugin-kernel/library.js
@@ -2,6 +2,8 @@
 
 var plugin = {},
 	net = require('net'),
+	jsdom = require("jsdom"),
+	fivebeans = require('fivebeans'),
 	winston = module.parent.require('winston'),
 	async = module.parent.require('async'),
 	topics = module.parent.require('./topics'),
@@ -44,10 +46,17 @@ plugin.topic = {};
 
 plugin.getTopics = function(data, callback) {
 	var topics = data.topics;
-
 	async.map(topics, function(topic, next) {
-		if(topic.status == 0) {
-			topic.title = '<span class="finished"><i class="fa fa-check"></i> 等待运算</span> ' + topic.title;
+		if(topic.status == 1) {
+			topic.title = '<span class="waiting"><i class="fa fa-clock-o"></i> 等待运算</span> ' + topic.title;
+		} else if(topic.status == 2) {
+			topic.title = '<span class="evaluate"><i class="fa fa-play"></i> 正在计算</span> ' + topic.title;
+		} else if(topic.status == 3) {
+			topic.title = '<span class="finished"><i class="fa fa-check"></i> 计算完成</span> ' + topic.title;
+		} else if(topic.status == -1) {
+			topic.title = '<span class="error"><i class="fa fa-remove"></i> 语法错误</span> ' + topic.title;
+		} else if(topic.status == -2) {
+			topic.title = '<span class="aborted"><i class="fa fa-exclamation"></i> 计算超时</span> ' + topic.title;
 		}
 		return next(null, topic);
 	}, function(err, topics) {
@@ -56,9 +65,7 @@ plugin.getTopics = function(data, callback) {
 };
 
 plugin.topic.post = function(topic, callback) {
-
 	callback = callback || function() {};
-
 	topics.getMainPost(topic.tid, topic.uid, function(err, post){
 		if(err) {
 			winston.error(err);
@@ -69,8 +76,45 @@ plugin.topic.post = function(topic, callback) {
 			winston.error(err);
 			return callback(err);
 		}
-		winston.info(post.content);
-		topics.setTopicField(topic.tid, 'status', 0, callback);
+		jsdom.env(
+			post.content,
+			['http://www.wiseker.com/vendor/jquery/js/jquery.js'],
+			function (err, window) {
+				if (err) {
+					window.close();
+					winston.error(err);
+					return callback(err);
+				}
+				var codes = window.$("code[class='language-mma']");
+				window.close();
+				if (codes && codes.length > 0) {
+					var beans = new fivebeans.client('localhost', 11300);
+					beans.on('connect', function () {
+						beans.use('kernel-topic', function (err) {
+							if (err) {
+								winston.error(err);
+								return callback(err)
+							}
+							beans.put(Math.pow(2, 32), 0, 120, JSON.stringify({tid: topic.tid, mainpid: topic.mainPid, action: 'create'}), function (err, jobid) {
+								beans.end();
+								if(err) {
+									winston.error(err);
+									return callback(err);
+								}
+								topics.setTopicField(topic.tid, 'status', 1, callback);
+							});
+						});
+				    }).on('error', function (err) {
+				    	winston.error(err);
+						return callback(err);
+				    }).connect();
+
+					
+				} else {
+					topics.setTopicField(topic.tid, 'status', 0, callback);
+				}
+			}
+		);
 	});
 };
 
