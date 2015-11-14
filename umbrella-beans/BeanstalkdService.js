a4832e1fe903c94520142eeef9fd7fa0f03ebb9b
fuhaiq <fuhaiq@gmail.com>
2015-11-14 10:26:50 +0800

    remove beans project from java as nodejs

---
+++ umbrella-beans/BeanstalkdService.js
@@ -0,0 +1,199 @@
+'use strict';
+
+var MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt({
+		'langPrefix': 'language-'
+	}),
+	fs = require('fs-extra'),
+	jquery = fs.readFileSync("./jquery-2.1.4.min.js", "utf-8"),
+	jsdom = require("jsdom"),
+	async = require('async'),
+	http = require('http');
+
+var BeanstalkdService = function(db, redis) {
+
+	var emit = function (pid, callback) {
+	    async.waterfall([
+	        function (callback) {
+	            db.collection("objects").findOne({_key:'post:' + pid}, callback);
+	        },
+	        function (post, callback) {
+	            db.collection("objects").findOne({_key:'topic:' + post.tid}, function (err, topic) {
+	                return callback(err, post, topic);
+	            });
+	        },
+	        function (post, topic, callback) {
+	        	if(post.pid == topic.mainPid) {
+	        		var message = {status: post.status, tid: topic.tid};
+		            io.to('category_' + topic.cid).emit('kernel:topic', message);
+		            io.to('recent_topics').emit('kernel:topic', message);
+		            io.to('popular_topics').emit('kernel:topic', message);
+		            io.to('unread_topics').emit('kernel:topic', message);
+	        	}
+	        	return callback(null, null);
+	        }
+	    ], callback);
+	};
+
+	var kernel = function (post, pid, dir, url, callback) {
+		async.waterfall([
+			function (callback) {
+				var html = md.render(post.content);
+				jsdom.env({
+		            html: html,
+		            src: [jquery],
+		            done: function (err, window) {
+		            	if(err) {
+		            		return callback(err);
+		            	}
+		                var codes = window.$("code[class='language-mma']");
+		                if(codes.length < 1) {
+		                	window.close();
+		                	return callback('回复[post:'+post.pid+']内容找不到执行脚本');
+		                }
+		                var scripts = []
+		                for(var i = 0; i < codes.length; i++) {
+		                	scripts.push(window.$(codes[i]).text())
+		                }
+		                window.close();
+		                return callback(null, scripts);
+		            }
+		        });
+			},
+			function (scripts, callback) {
+				db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:2}}, function (err) {
+					if(err) {
+						return callback(err);
+					}
+					emit(pid, function () {
+						fs.mkdirsSync(dir + pid);
+						var kernel = JSON.stringify({dir: dir + pid + '/', scripts:scripts});
+						var options = {
+					        path: url,
+					        method: 'POST',
+					        headers: {
+					            'Content-Type': 'application/json;charset=UTF-8',
+					            'Content-Length': Buffer.byteLength(kernel, 'utf8')
+					        }
+					    };
+					    var request = http.request(options, function(response) {
+					        response.setEncoding('utf8');
+					        response.on('data', function(chunk) {
+					        	var act = JSON.parse(chunk);
+					            switch(response.statusCode) {
+					                case 200 :
+					                	var lastData = act[act.length - 1];
+							        	var status = 3;
+							        	if(lastData.type == 'error') {
+							        		status = -1;
+							        	} else if(lastData.type == 'abort') {
+							        		status = -2;
+							        	}
+					                	db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:status, result: act}}, function (err) {
+					                		return callback(err, false);
+					                	});
+					                    break;
+					                case 502 :
+					                	db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:1}}, function (err) {
+					                		return callback(err, true);
+					                	});
+					                    break;
+					                case 500 :
+					                    if(string(act.exception).contains('java.util.NoSuchElementException')) {
+					                    	db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:1}}, function (err) {
+						                		return callback(err, true);
+						                	});
+					                        break;
+					                    } else if(string(act.exception).contains('java.util.concurrent.TimeoutException')) {
+					                        db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:-2}}, function (err) {
+						                		return callback(err, false);
+						                	});
+					                        break;
+					                    } else {
+					                    	db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:1}}, function (err) {
+						                		return callback(err, true);
+						                	});
+					                        break;
+					                    }
+					                default :
+					                	db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:1}}, function (err) {
+					                		return callback(err, true);
+					                	});
+					            }
+					        });
+					    });
+					    request.write(kernel);
+					    request.end();
+					})
+				});
+			}
+		], function (err, needRelease) {
+			if(err) {
+				db.collection("objects").updateOne({_key:'post:' + pid}, {$set:{status:1}}, function () {
+            		emit(pid, function () {
+            			return callback(err);
+					});
+            	});
+			} else {
+				emit(pid, function () {
+					return callback(null, needRelease);
+				})
+			}
+		}); // end of waterfall
+	};
+
+	var db = db;
+
+	var io = require('socket.io-emitter')(redis);
+
+	this.update = function (jobData, callback) {
+		var dir = jobData.dir,
+			pid = jobData.pid,
+			url = jobData.url;
+		db.collection("objects").findOne({_key:'post:' + pid}, function (err, post) {
+			if(err) {
+				return callback(err);
+			}
+			if(!post) {
+				return callback('回复[post:'+pid+']不存在');
+			}
+			if(post.status == 0) {
+				fs.removeSync(dir + pid);
+				return callback(null, false);
+			} else if(post.status == 1) {
+				fs.removeSync(dir + pid);
+				return kernel(post, pid, dir, url, callback);
+			} else {
+				return callback('回复[post:'+post.pid+']状态错误['+post.status+'], 期望值: 1或0');
+			}
+		});
+	};
+
+	this.purge = function (jobData, callback) {
+		var dir = jobData.dir,
+			pid = jobData.pid;
+		fs.removeSync(dir + pid);
+		return callback(null, false);
+	};
+
+	this.create = function (jobData, callback) {
+		var dir = jobData.dir,
+			pid = jobData.pid,
+			url = jobData.url;
+		db.collection("objects").findOne({_key:'post:' + pid}, function (err, post) {
+			if(err) {
+				return callback(err);
+			}
+			if(!post) {
+				return callback('回复[post:'+pid+']不存在');
+			}
+			if(post.status != 1) {
+				return callback('回复[post:'+post.pid+']状态错误['+post.status+'], 期望值: 1');
+			}
+			return kernel(post, pid, dir, url, callback);
+		});
+	};
+
+}
+
+module.exports = BeanstalkdService;
---
+++ umbrella-beans/app.js
@@ -0,0 +1,188 @@
+'use strict';
+
+var log4js = require('log4js');
+log4js.configure({
+	appenders: [{
+        type: "console"
+    }],
+    replaceConsole: true
+});
+global.LOG = log4js.getLogger();
+
+var fs = require('fs-extra'),
+    jquery = fs.readFileSync("./jquery-2.1.4.min.js", "utf-8"),
+    config = fs.readJsonSync("./config.json"),
+    jsdom = require("jsdom"),
+    MongoClient = require('mongodb').MongoClient,
+    redis = require('redis'),
+    async = require('async'),
+    BeanstalkdService = require("./BeanstalkdService"),
+    fivebeans = require('fivebeans'),
+    string = require('string'),
+    db = null,
+    redisClient = null,
+    beanstalkd = null,
+    service = null;
+
+var reserve = function () {
+    async.waterfall([
+        function (callback) {
+            LOG.info('开始接收beanstalkd任务');
+            beanstalkd.reserve(callback);
+        },
+        function (jobid, payload, callback) {
+            var jobData = JSON.parse(payload.toString());
+            jobData.dir = config.imgDir;
+            jobData.url = config.kernelUrl;
+            if(!string(jobData.pid).isNumeric()) {
+                return callback('回复pid为不是数字', null);
+            }
+            if(string(jobData.dir).isEmpty()) {
+                return callback('回复文件路径dir为空', null);
+            }
+            if(string(jobData.url).isEmpty()) {
+                return callback('计算url为空', null);
+            }
+            return callback(null, jobid, jobData);
+        },
+        function (jobid, jobData, callback) {
+            redisClient.setnx('post:lock:' + jobData.pid, 'locked' , function (err, buffer) {
+                if(err) {
+                    return callback(err);
+                }
+                var locked = buffer.toString();
+                if(locked == 0) {
+                    LOG.warn('回复[post:'+jobData.pid+']被锁定,延时5秒后重新调度任务[jobid:'+jobid+']');
+                    beanstalkd.release(jobid, Math.pow(2, 32), 5, callback);
+                } else {
+                    async.waterfall([
+                        function (callback) {
+                            var fn = null;
+                            switch(jobData.action) {
+                                case 'create' : 
+                                    fn = service.create;
+                                    break;
+                                case 'update' :
+                                    fn = service.update;
+                                    break;
+                                case 'purge' :
+                                    fn = service.purge;
+                                    break;
+                                default :
+                                    LOG.warn('没有此类型['+jobData.action+']任务,直接删除');
+                                    fn = function (jobData, callback) {
+                                        return callback(null, false);
+                                    };
+                            }
+                            return fn(jobData, callback);
+                        },
+                        function (needRelease, callback) {
+                            if(needRelease) {
+                                LOG.warn('回复[post:'+jobData.pid+']未操作,延时5秒后重新调度任务[jobid:'+jobid+']')
+                                beanstalkd.release(jobid, Math.pow(2, 32), 5, callback);
+                            } else {
+                                LOG.info('完成操作,删除任务')
+                                beanstalkd.destroy(jobid, callback);
+                            }
+                        }
+                    ], function (err) {
+                        redisClient.del('post:lock:' + jobData.pid, function () {
+                            if(err) {
+                                LOG.error('操作出错,bury任务[jobid:'+jobid+']')
+                                beanstalkd.bury(jobid, Math.pow(2, 32), function () {
+                                    return callback(err);
+                                });
+                            } else {
+                                return callback(null, null);
+                            }
+                        });
+                    });//end of waterfall
+                }//end of else, means the post is locked.
+            });//end of redis.setnx, try lock.
+        }//end of function
+    ], function (err) {
+        if(err) {
+            LOG.error(err);
+        }
+        reserve();
+    });
+};
+
+async.parallel({
+    mongodb: function (callback) {
+        MongoClient.connect(config.mongo.url, function (err, db) {
+            if(err) {
+                LOG.error('链接mongodb出错:');
+                return callback(err);
+            }
+            return callback(null, db);
+        });
+    },
+    redis: function (callback) {
+        var client = redis.createClient(config.redis.port, config.redis.host, {
+            return_buffers : true
+        });
+        client.on("connect", function (){
+            return callback(null, client);
+        });
+        client.on("error", function (err) {
+            LOG.error('链接redis出错:');
+            return callback(err);
+        });
+    },
+    beanstalkd: function (callback) {
+        var client = new fivebeans.client(config.beanstalkd.host, config.beanstalkd.port);
+        client.on('connect', function () {
+            return callback(null, client);
+        })
+        .on('error', function (err) {
+            LOG.error('链接beanstalkd出错:');
+            return callback(err);
+        })
+        .connect();
+    }
+},function (err, results){
+    db = results.mongodb;
+    redisClient = results.redis;
+    beanstalkd = results.beanstalkd;
+    if(err) {
+        LOG.error(err);
+    } else {
+        service = new BeanstalkdService(db, redisClient);
+        beanstalkd.watch(config.beanstalkd.tube, function (err) {
+            if(err) {
+                LOG.error(err);
+            } else {
+                reserve();
+            }
+        });
+    }
+});
+
+/* shutdown event for pm2 */
+process.on('SIGTERM', function() {
+    if(db) {
+        db.close();
+    }
+    if(redisClient) {
+        redisClient.quit();
+    }
+    if(beanstalkd) {
+        beanstalkd.quit();
+    }
+    process.exit(0)
+});
+
+/* shutdown event for Ctrl+C */
+process.on('SIGINT', function() {
+    if(db) {
+        db.close();
+    }
+    if(redisClient) {
+        redisClient.quit();
+    }
+    if(beanstalkd) {
+        beanstalkd.quit();
+    }
+    process.exit(0)
+});
