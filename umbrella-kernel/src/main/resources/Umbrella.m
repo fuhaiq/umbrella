2379bd4f2e0f6316f5001e269699b541c3b0fda0
fuhaiq <fuhaiq@gmail.com>
2015-11-27 16:34:12 +0800

    add security check in m file

--- umbrella-kernel/src/main/resources/Umbrella.m
+++ umbrella-kernel/src/main/resources/Umbrella.m
@@ -13,7 +13,24 @@ Begin["`Private`"]
 Needs["JLink`"]
 
 (* Set MathMLForm *)
-$PrePrint = With[{expr = #},If[MemberQ[expr, _Graphics | _Graphics3D | _Graph | _Manipulate | _Rotate, {0, \[Infinity]}], LinkWrite[$ParentLink, DisplayPacket[EvaluateToTypeset[#]]], MathMLForm[expr]]] &;
+
+$PrePrint = With[{expr = #},If[MemberQ[expr, _Graphics | _Graphics3D | _Graph | _Manipulate | _Rotate, {0, \[Infinity]}], LinkWrite[$ParentLink, DisplayPacket[EvaluateToTypeset[expr]]], MathMLForm[expr]]] &;
+
+
+(*
+
+Needs["MSP`"]
+
+MSP`Utility`SetSecurity[ "SecurityConfiguration.m"];
+
+$Pre = Function[expr, 
+      Module[
+        {expr$ = ToString[Unevaluated[expr], InputForm]},
+         MSPToExpression[expr$]
+  ],
+  HoldAll
+]
+*)
 
 (* Set time constrained *)
 SetAttributes[timecon, HoldAll]
