41419efa17de928f59af29a42e517938891a9e20
fuhaiq <fuhaiq@gmail.com>
2025-02-04 22:22:22 +0800

    V20250204-添加旅店逻辑

--- umbrella-core/umbrella-business/umbrella-user/src/main/java/org/umbrella/user/service/impl/TbHotelServiceImpl.java
+++ umbrella-core/umbrella-business/umbrella-user/src/main/java/org/umbrella/user/service/impl/TbHotelServiceImpl.java
@@ -16,5 +16,5 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TbHotelServiceImpl extends ServiceImpl<TbHotelMapper, TbHotel> implements ITbHotelService {
-
+    //TODO: 添加旅店逻辑
 }
